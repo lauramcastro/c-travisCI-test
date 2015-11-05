4214e38963bf9d13c62e7fdfb2854f0064f78847
Laura M. Castro <lcastro@udc.es>
2015-11-05 19:06:29 +0100

    Simple 'hello world' in C to test TravisCI support for this language.

---
+++ Makefile
@@ -0,0 +1,14 @@
+all: lcov cunit
+
+cunit: holamundo.o holamundo-test.c
+	gcc -Wall -Werror --coverage -o holamundo-test holamundo.o holamundo-test.c -lcunit
+
+lcov: holamundo.o 
+	lcov --capture --directory . --output-file coverage.info
+	genhtml coverage.info --output-directory coverage	
+
+holamundo.o: holamundo.h holamundo.c
+	gcc -Wall -Werror -c --coverage holamundo.c
+
+clean:
+	rm -rf *~ *.o core holamundo holamundo-test *.tst coverage coverage.info *.gcno *.gcda
---
+++ holamundo.c
@@ -0,0 +1,30 @@
+/*
+  FILE: holamundo.c
+  AUTHOR: Laura M. Castro <lcastro@udc.es> (c) 2013
+  LICENSE:
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "holamundo.h"
+#include <stdlib.h>
+
+char *saludar()
+{
+  char *saludo = NULL;
+
+  saludo = (char *) calloc(14, sizeof(char));
+  saludo = "¡Hola, mundo!";
+
+  return saludo;
+}
